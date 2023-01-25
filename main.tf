350f505b3f74fcf361d0e0c4bd864e4a4d9feb8b
Priti <61485216+pritiranga@users.noreply.github.com>
2023-01-25 06:26:30 +0000

    edit main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@
 resource "aws_instance" "dev_terra" {
   ami           = var.ami
   instance_type = var.instance_type
-  region = var.region
-  key = var.key 
-
+  
   tags = {
     Env = "dev"
   }
