1d9c68910b86b53f55583e4620cd532c1fae4027
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-02 10:51:39 +0000

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,3 @@
-terraform {
-
-  required_providers {
-
-    docker = {
-
-      source  = "kreuzwerker/docker"
-
-      version = "3.0.1"
-
-    }
-
-    aws = {
-
-      source  = "hashicorp/aws"
-
-      version = "~> 4.16"
-
-    }
-
-  }
-
-}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
-
-
 #creating ec2 instance 
 # resource "aws_instance" "dev_terra" {
 #   ami           = var.ami
