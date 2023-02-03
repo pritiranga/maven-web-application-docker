84174c812e3014b51d798db5797f801d09695ffe
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-03 08:24:42 +0000

    m

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "TF-key" {
+resource "aws_key_pair" "demo-key" {
     key_name = var.key
     public_key = tls_private_key.rsa.public_key_openssh
 }
@@ -8,7 +8,7 @@ resource "tls_private_key" "rsa" {
     rsa_bits = 4096
 }
 
-resource "local_file" "TF_key" {
+resource "local_file" "save-key" {
     content = tls_private_key.rsa.private_key_pem
     filename = var.private_key
 }
