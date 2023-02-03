0344dad53bd433759c683e4f8424b2426d3887c3
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-03 09:47:57 +0000

    kry

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-resource "aws_key_pair" "demo-key" {
-    key_name = var.key
-    public_key = tls_private_key.rsa.public_key_openssh
-}
+# resource "aws_key_pair" "demo-key" {
+#     key_name = var.key
+#     public_key = tls_private_key.rsa.public_key_openssh
+# }
 
-resource "tls_private_key" "rsa" {
-    algorithm = "RSA"
-    rsa_bits = 4096
-}
+# resource "tls_private_key" "rsa" {
+#     algorithm = "RSA"
+#     rsa_bits = 4096
+# }
 
-resource "local_file" "save-key" {
-    content = tls_private_key.rsa.private_key_pem
-    filename = var.private_key
-}
+# resource "local_file" "save-key" {
+#     content = tls_private_key.rsa.private_key_pem
+#     filename = var.private_key
+# }
 
 resource "aws_instance" "ec2" {
     ami = var.ami 
