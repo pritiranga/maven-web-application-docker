bc38664d2dd077a8eb29889678857e3154a9ad6a
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-06 10:36:57 +0000

    add key in main.tf

--- main.tf
+++ main.tf
@@ -70,10 +70,19 @@ resource "aws_security_group" "demosg" {
   }
 }
 
-# Creating key pair
-resource "aws_key_pair" "demokey" {
-  key_name   = "var.key"
-  public_key = "${file(var.public_key)}"
+resource "aws_key_pair" "demo-key" {
+    key_name = var.key
+    public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+resource "local_file" "save-key" {
+    content = tls_private_key.rsa.private_key_pem
+    filename = var.private_key
 }
 
 # Creating Instances
