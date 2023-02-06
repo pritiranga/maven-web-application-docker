c61484fc8de8eb6403c6b198c2256354940735ac
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-06 11:14:06 +0000

    jkhk

--- main.tf
+++ main.tf
@@ -1,75 +1,3 @@
-# Creating VPC
-resource "aws_vpc" "demovpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "Demo VPC"
-  }
-}
-
-# Creating Internet Gateway 
-resource "aws_internet_gateway" "demogateway" {
-  vpc_id = "${aws_vpc.demovpc.id}"
-}
-
-# Grant the internet access to VPC by updating its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.demovpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.demogateway.id}"
-}
-
-# Creating 1st subnet 
-resource "aws_subnet" "demosubnet" {
-  vpc_id                  = "${aws_vpc.demovpc.id}"
-  cidr_block             = "${var.subnet_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Demo subnet"
-  }
-}
-
-# Creating 2nd subnet 
-resource "aws_subnet" "demosubnet1" {
-  vpc_id                  = "${aws_vpc.demovpc.id}"
-  cidr_block             = "${var.subnet1_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Demo subnet 1"
-  }
-}
-
-# Creating Security Group
-resource "aws_security_group" "demosg" {
-  name        = "Demo Security Group"
-  description = "Demo Module"
-  vpc_id      = "${aws_vpc.demovpc.id}"
-
-  # Inbound Rules
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    #cidr_blocks = ["0.0.0.0/0"]
-    self = true
-  }
-
-  # Outbound Rules
-  # Internet access to anywhere
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # Creating key-pair
 resource "aws_key_pair" "demo-key" {
     key_name = var.key
@@ -91,7 +19,7 @@ resource "aws_instance" "terra-staging" {
 
   # AMI based on region 
   ami = var.ami
-
+  vpc_security_group_ids = ["sg-0426fcc5469e1658e"]
 
   # Launching instance into subnet 
   subnet_id = "${aws_subnet.demosubnet.id}"
