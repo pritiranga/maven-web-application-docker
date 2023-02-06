fe38bcbf62c15cb54d482c0a49e64dbb6ee36bbb
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-06 10:59:33 +0000

    jh

--- provider.tf
+++ provider.tf
@@ -1,25 +1,25 @@
 # Declaring the Required provider (Docker provider)
 terraform {
   required_providers {
-    docker = {
-        source = "kreuzwerker/docker"
-        version = "3.0.1"
-    }
+    # docker = {
+    #     source = "kreuzwerker/docker"
+    #     version = "3.0.1"
+    # }
 
     aws = {
         source  = "hashicorp/aws"
-        version = "~> 4.16"
+        version = "4.16"
 
     }
   }
 }
 
 # Specifying the Docker provider configuration
-provider "docker" {
-    host = "unix:///var/run/docker.sock"
-    #host ="unix:///home/user/.docker/desktop/docker.sock"
+# provider "docker" {
+#     host = "unix:///var/run/docker.sock"
+#     #host ="unix:///home/user/.docker/desktop/docker.sock"
 
-}
+# }
 
 provider "aws" {
     region = var.region
