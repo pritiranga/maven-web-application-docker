f094591d8a78bb14aae4609a912fe6c924c78d1d
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-08 16:35:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,135 +1,76 @@
-# Creating key-pair
-resource "aws_key_pair" "demo-key" {
-    key_name = var.key
-    public_key = tls_private_key.rsa.public_key_openssh
+// Key pair
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-resource "tls_private_key" "rsa" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.default.key_name}.pem"
+  content = tls_private_key.key.private_key_pem
 }
 
-resource "local_file" "save-key" {
-    content = tls_private_key.rsa.private_key_pem
-    filename = var.private_key
+resource "aws_key_pair" "default" {
+  key_name   = var.key
+  public_key = tls_private_key.key.public_key_openssh 
 }
 
-# Creating Instances
-resource "aws_instance" "terra-staging" {
-
-  # AMI based on region 
-  ami = var.ami
-  vpc_security_group_ids = ["sg-0426fcc5469e1658e"]
 
 
-  # Instance type 
-  instance_type = var.instance_type
-  
-  # Count of instance
-   count= 1
-  
-  # SSH key that we have generated above for connection
-  key_name = var.key
-
-  # Attaching Tag to Instance 
-  tags = {
-    Name = "Staging-Instance with Terraform"
+//Security Group
+resource "aws_security_group" "sg" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-     
-#     # SSH into instance 
-#   connection {
-#     type        = "ssh"
-#     host        = self.public_ip
-#     user        = "ubuntu"
-#     private_key = "${file("~/.ssh/id_rsa")}"
-#     timeout     = "60s"
-#   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// EC2
+resource "aws_instance" "staging" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.default.key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "Staging-docker"
+  }
+
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.key.private_key_pem
+    host        = self.public_ip
+  }
   
-#   # Installing splunk & creating distributed indexer clustering on newly created instance
-#   provisioner "remote-exec" {
-#       inline = [
-#         "sudo apt update -y",
-#         "sudo groupadd docker",
-#         "sudo usermod -aG docker $USER",
-#         "sudo newgrp docker",
-#         "sudo apt install docker.io",
-#         "sudo chmod 666 /var/run/docker.sock",
-#         "cd maven-project",
-#         "docker build -t pritidevops/web-app .",
-#         "docker run -itd -p 8080:8080 pritidevops/web-app --name webapp"
-#   ]
-#  }
+  provisioner "file" {
+    source      = "user_data.sh"
+    destination = "/home/ubuntu/user_data.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/user_data.sh",
+      "sudo /home/ubuntu/user_data.sh"
+    ]
+  }
 }
-
-resource “null_resource” “example” {
-
-  provisioner “remote-exec” {
-
-    connection {
-
-      type = "ssh"
-
-      user = "ubuntu"
-
-      private_key = file("~/.ssh/id_rsa")
-
-      host = aws_instance.terra-staging.public_ip
-
-      file = file("~/.ssh/id_rsa")
-
-}
-}
-}
-
-
-# data "aws_security_group" "demosg" {
-#     id = "sg-06f293b32926c8afc"  
-# }
-
-# resource "aws_instance" "ec2" {
-#     ami = var.ami 
-#     instance_type = var.instance_type
-#     key_name= var.key
-#     # security_groups = data.aws_security_group.demosg.id
-#     vpc_security_group_ids = [data.aws_security_group.demosg.id]
-
-#     # Attaching Tag to Instance 
-#     tags = {
-#         Name = "Demo-terra-Instance"
-#     }
-
-#     # SSH into instance 
-#     connection {
-        
-#         # The default username for our AMI
-#         user = "ubuntu"
-        
-#         # Private key for connection
-#         private_key = file("./keys/task-demo.pem")
-        
-#         # Type of connection
-#         type = "ssh"
-
-#         host = self.public_ip
-#     }
-
-#     # Installing docker on newly created instance
-#     provisioner "remote-exec" {
-#         inline = [
-#             "sudo apt update -y",
-#             "sudo groupadd docker",
-#             "sudo usermod -aG docker $USER",
-#             "sudo newgrp docker",
-#             "sudo apt install docker.io",
-#             "docker --version",
-#             "docker build -t webapp .",
-#             "sudo chmod 666 /var/run/docker.sock",
-#             # "docker pull pritidevops/webapp:latest .",
-#             # "docker run -itd -p 8080:8080 pritidevops/webapp:latest --name web-app"
-            
-#         ]
-#     }
-# }
 
 
 
