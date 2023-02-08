0f68b5148701437d0571b915328ee72f7d2287d3
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-08 16:35:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-files-jenkins-demo"
-    key = "task-demo"
+    bucket = "terraform-jenkins-local"
+    key = "terraform.tfstate"
     region = "us-east-1"
   }
 }
