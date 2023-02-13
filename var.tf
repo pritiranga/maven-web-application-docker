5fd00fbe3e043c2e7e4dac678969f0bd63ab9274
Priti <61485216+pritiranga@users.noreply.github.com>
2023-02-13 16:02:10 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,9 +1,14 @@
-# variable "ecr_name" {
-#   default = "demo-web-repo"
-# }
+variable "repo_name" {
+  default = "demo-web-repo"
+}
 
 variable "region" {
   default = "us-east-1"
 }
+
+variable "aws_account_id" {
+  default = "109968515111"
+}
+
 
 
